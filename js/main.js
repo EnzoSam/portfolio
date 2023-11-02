4d80cbee9024709855356b275007f1978b7f2556
Enzo Samudio <enzosamudio@gmail.com>
2023-11-02 18:07:34 -0300

    Armado de estructura de archivos

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+    console.log('ready');
+    $('.navbar_wraper').load('./components/navbar_wraper.html');    
+    $('.footer_wraper').load('./components/footer_wraper.html');
+    
+});
