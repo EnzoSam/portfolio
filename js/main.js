99243a9eb3faa59e021dd13ac113bfe3b3a2f542
enzo256 <enzosamudio@gmail.com>
2025-07-22 10:11:43 -0300

    Rediseño total, agregado de proyecto mascovita

--- js/main.js
+++ js/main.js
@@ -1,30 +1,33 @@
-document.addEventListener("DOMContentLoaded", async function(event) 
-{
-
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+const themeToggleBtn = document.getElementById('theme-toggle');
+const themeToggleBtnMobile = document.getElementById('theme-toggle-mobile');
+const htmlEl = document.documentElement;
+menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
 });
-
-export function load(url, elementId)
-{
-    return new Promise((resolve, reject)=>{
-        try{
-    const element = document.getElementById(elementId);
-    if(!element)
-        return;
-
-    fetch(url)
-    .then(response => response.text())
-    .then(data => {
-        element.innerHTML = data;
-        resolve();
-    })
-    .catch(error => {
-      console.error('Error al cargar el contenido:', error);
-      reject();
+const mobileLinks = mobileMenu.getElementsByTagName('a');
+for (let i = 0; i < mobileLinks.length; i++) {
+    mobileLinks[i].addEventListener('click', () => {
+        mobileMenu.classList.add('hidden');
     });
+}
+const toggleTheme = () => {
+    htmlEl.classList.toggle('dark');
+    htmlEl.classList.toggle('light');
+    if (htmlEl.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
     }
-    catch(exc)
-    {
-        reject(exc);
-    }
-    });
+};
+themeToggleBtn.addEventListener('click', toggleTheme);
+themeToggleBtnMobile.addEventListener('click', toggleTheme);
+// Check for saved theme preference
+if (localStorage.getItem('theme') === 'light') {
+    htmlEl.classList.remove('dark');
+    htmlEl.classList.add('light');
+} else {
+    htmlEl.classList.remove('light');
+    htmlEl.classList.add('dark');
 }
